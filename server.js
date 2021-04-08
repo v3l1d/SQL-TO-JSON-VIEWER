5a8aacb8e34cf5aec331a5bea18c83b5e5b3ce21
lorehaze <lorenzo.baldari.98@gmail.com>
2021-04-08 18:16:02 +0200

    Node Changes
    Installed NodeJS,
    set up server,
    installed sql-dd-parser

---
+++ server.js
@@ -0,0 +1,35 @@
+const port = 3000;
+var express = require('express')
+var fs = require('fs')
+const { Parser } = require('sql-ddl-to-json-schema');
+
+const parser = new Parser('mysql');
+var app = express()
+
+
+app.get('/', function(req, res) {
+    fs.readFile('./storage/app/uploads/TestTables.sql' + req.query.file, 'utf8', function (err,sql) {
+        console.log(req.query.file);
+        if (err) {
+            return console.log(err);
+        }
+        console.log(sql);
+        const options = {};
+        parser.feed(sql);
+        const parsedJsonFormat = parser.results;
+        const compactJsonTablesArray = parser.toCompactJson(parsedJsonFormat);
+        //console.log(JSON.stringify(compactJsonTablesArray, null, 2));
+        for (var elem in compactJsonTablesArray) {
+            console.log(JSON.stringify(elem));
+        }
+        res.writeHead(200, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' });
+        res.end(JSON.stringify(compactJsonTablesArray));
+    });
+    var file = req.query.file
+    //res.send(file);
+})
+
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`)
+})
