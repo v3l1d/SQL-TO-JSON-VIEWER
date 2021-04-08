18060b3115308a6ba3160c59e18682a68a33dab6
lorehaze <lorenzo.baldari.98@gmail.com>
2021-04-08 12:31:38 +0200

    restoring

---
+++ app/Http/Controllers/fileUploadController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class fileUploadController extends Controller
+{
+    public function upload() {
+        return view('welcome');
+    }
+}
