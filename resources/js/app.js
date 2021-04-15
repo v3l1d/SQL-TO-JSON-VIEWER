f20df6faa043aafee13d0db6a8b311e1ffb7ab55
Alessandro Dilevrano <57458926+v3l1d@users.noreply.github.com>
2021-04-15 18:32:17 +0200

    changes

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,5 @@
 require('./bootstrap');
+require('./jquery-3.6.0.js');
+require('./jquery-ui-1.12.1/jquery-ui-min.js')
+require('./jquery-ui-1.12.1/jquery-ui.js')
+require('./jquery.flowchart.js'');
